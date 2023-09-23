b81a30d08a5d005a3fe1262d2d35c18f7589e021
anhtuuuu <anhtu18042004@gmail.com>
2023-09-23 17:54:19 +0700

    update portfolio

---
+++ layout/contents/portfolio/breadcrumbs.php
@@ -0,0 +1,16 @@
+<div class="breadcrumbs_area">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="breadcrumb_content">
+                                            <ul>
+                                                <li><a href="<?php echo $level?>index.php">home</a></li>
+                                                <li><i class="fa fa-angle-right"></i></li>
+                                                <li>portfolio</li>
+                                            </ul>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
---
+++ layout/meta-data/meta.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Coron-portfolio</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="<?php echo $level?><?php echo $level?>assets\img\favicon.png">
+		
+		<!-- all css here -->
+       <link rel="stylesheet" href="<?php echo $level?>assets\css\bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo $level?>assets\css\plugin.css">
+        <link rel="stylesheet" href="<?php echo $level?>assets\css\bundle.css">
+        <link rel="stylesheet" href="<?php echo $level?>assets\css\style.css">
+        <link rel="stylesheet" href="<?php echo $level?>assets\css\responsive.css">
+        <script src="<?php echo $level?>assets\js\vendor\modernizr-2.8.3.min.js"></script>
